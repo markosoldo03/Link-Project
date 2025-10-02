988468b3df08a64ded9f78c5d27532da59e7d406
markosoldo03 <markosoldo03@gmail.com>
2025-10-02 23:39:09 +0200

    Link

--- Link/src/App.tsx
+++ Link/src/App.tsx
@@ -1,13 +1,22 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import Block from './components/block'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <p> bok</p>
+    <>
+
+      
+      <header className='w-screen bg-gray-700 h-20 text-4xl text-white flex  font-bold'>
+        <img src="Logo.png" className=' block m-0 w-80 h-auto  '></img> </header>
+      <div className='w-screen h-auto flex flex-row'>
+      <Block image="https://www.hdz.hr/photos/thumb/default.jpg"  title='Stranka te prati'/>
+      <Block image="https://www.sdp.ba/datoteke/uploads/sdp-bih-logo-1.jpg"  title='Sve je ona meni' />
+      <Block image="https://www.hrvatski-fokus.hr/wp-content/uploads/2024/04/HZV.jpg"  title='Miki milane'/>
+      <Block image="https://mozemo.hr/wp-content/uploads/2025/04/mozemo.jpg"  title='Bok stari'/>
+      
+      </div>
+    </>
   )
 }
 
---
+++ Link/src/components/block.tsx
@@ -0,0 +1,22 @@
+import React from 'react'
+type BlockProps = {
+  image: string;
+  title?: string;
+} & React.HTMLAttributes<HTMLDivElement>;
+
+export default function Block({
+image,
+title
+}: BlockProps) 
+ {
+    return(
+        <div className='w-1/3 h-auto bg-gray-700 flex m-3 flex-col '>
+      <div className='w-full h-3/4 overflow-hidden shrink-0 pr-2 '>
+          <img className='w-full h-full object-cover m-1 rounded-lg ' src={image}></img>
+          </div>
+          <div className=' justify-center py-5 flex items-center '>
+            <p className='font-bold text-xl mb-2'>{title}</p>
+          </div>
+      </div>
+    )
+}
