6f16f5a8588ffcbff2be77e43d53d1a226a48db0
markosoldo03 <markosoldo03@gmail.com>
2025-10-08 00:51:18 +0200

    Link

---
+++ tailwind.config.js
@@ -0,0 +1,7 @@
+ /** @type {import('tailwindcss').Config} */
+export default {
+  darkMode: 'class',
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}', './src/views/**/*.{js,ts,jsx,tsx}', './src/components/**/*.{js,ts,jsx,tsx}', './src/layouts/**/*.{js,ts,jsx,tsx}'],
+  theme: { extend: {} },
+  plugins: [],
+}
